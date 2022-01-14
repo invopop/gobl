bfc8d39f770bf962e0bad6922adf94ef7803fabd
Jonathan Hall <flimzy@flimzy.com>
2022-01-14 14:51:24 +0100

    gobl CLI skeleton

---
+++ cmd/gobl/main.go
@@ -0,0 +1,32 @@
+// The gobl command provides a command-line interface to the GOBL library.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	if err := run(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	root := &cobra.Command{
+		Use:           "gobl",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+	}
+	return root.ExecuteContext(ctx)
+}
