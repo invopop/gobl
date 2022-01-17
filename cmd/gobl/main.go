dc873a547378d9576fbb5c7cfbc8e85bb98d64dd
Jonathan Hall <flimzy@flimzy.com>
2022-01-17 20:27:43 +0100

    Start building the 'build' command

--- cmd/gobl/main.go
+++ cmd/gobl/main.go
@@ -32,40 +32,59 @@ func run() error {
 		SilenceErrors: true,
 	}
 	root.AddCommand(&cobra.Command{
-		Use:  "build",
-		RunE: build,
+		Use:  "verify [infile]",
+		Args: cobra.MaximumNArgs(1),
+		RunE: verify,
 	})
 	root.AddCommand(&cobra.Command{
-		Use:  "verify",
-		RunE: verify,
+		Use:  "build [infile] [outfile]",
+		Args: cobra.MaximumNArgs(2),
+		RunE: build,
 	})
 	return root.ExecuteContext(ctx)
 }
 
-func build(*cobra.Command, []string) error {
-	return nil
-}
-
-func verify(cmd *cobra.Command, args []string) error {
+func readEnv(cmd *cobra.Command, args []string) (*gobl.Envelope, error) {
 	input := cmd.InOrStdin()
 	if len(args) > 0 && args[0] != "-" {
 		f, err := os.Open(args[0])
 		if err != nil {
-			return err
+			return nil, err
 		}
 		defer f.Close() // nolint:errcheck
 		input = f
 	}
 	in, err := ioutil.ReadAll(input)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	env := new(gobl.Envelope)
 	if err := yaml.Unmarshal(in, env); err != nil {
-		return err
+		return nil, err
 	}
 	if err := env.Validate(); err != nil {
+		return nil, err
+	}
+	return env, nil
+}
+
+func verify(cmd *cobra.Command, args []string) error {
+	env, err := readEnv(cmd, args)
+	if err != nil {
 		return err
 	}
+
 	return env.Verify()
+}
+
+func build(cmd *cobra.Command, args []string) error {
+	env, err := readEnv(cmd, args)
+	if err != nil {
+		return err
+	}
+	doc := new(gobl.Document)
+	if err := env.Extract(doc); err != nil {
+		return err
+	}
+	return nil
 }
--- cmd/gobl/main_test.go
+++ cmd/gobl/main_test.go
@@ -111,3 +111,92 @@ func Test_verify(t *testing.T) {
 		})
 	}
 }
+
+func Test_build(t *testing.T) {
+	tests := []struct {
+		name string
+		in   io.Reader
+		args []string
+		err  string
+	}{
+		{
+			name: "invalid stdin",
+			in:   strings.NewReader("this isn't JSON"),
+			err:  "error unmarshaling JSON: json: cannot unmarshal string into Go value of type gobl.Envelope",
+		},
+		{
+			name: "success",
+			in: func() io.Reader {
+				f, err := os.Open("testdata/nototals.json")
+				if err != nil {
+					t.Fatal(err)
+				}
+				t.Cleanup(func() {
+					_ = f.Close()
+				})
+				return f
+			}(),
+		},
+		// {
+		// 	name: "digest mismatch",
+		// 	in: func() io.Reader {
+		// 		f, err := os.Open("testdata/digest-mismatch.json")
+		// 		if err != nil {
+		// 			t.Fatal(err)
+		// 		}
+		// 		t.Cleanup(func() {
+		// 			_ = f.Close()
+		// 		})
+		// 		return f
+		// 	}(),
+		// 	err: "digest mismatch",
+		// },
+		// {
+		// 	name: "read from file",
+		// 	args: []string{"testdata/digest-mismatch.json"},
+		// 	err:  "digest mismatch",
+		// },
+		// {
+		// 	name: "file missing",
+		// 	args: []string{"asdf"},
+		// 	err:  "open asdf: no such file or directory",
+		// },
+		// {
+		// 	name: "explicit stdin",
+		// 	args: []string{"-"},
+		// 	in: func() io.Reader {
+		// 		f, err := os.Open("testdata/digest-mismatch.json")
+		// 		if err != nil {
+		// 			t.Fatal(err)
+		// 		}
+		// 		t.Cleanup(func() {
+		// 			_ = f.Close()
+		// 		})
+		// 		return f
+		// 	}(),
+		// 	err: "digest mismatch",
+		// },
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			c := &cobra.Command{}
+			if tt.in != nil {
+				c.SetIn(tt.in)
+			}
+			buf := &bytes.Buffer{}
+			c.SetOut(buf)
+			err := build(c, tt.args)
+			if tt.err != "" {
+				assert.EqualError(t, err, tt.err)
+			} else {
+				assert.Nil(t, err)
+			}
+			if d := testy.DiffText(testy.Snapshot(t), buf.String()); d != nil {
+				t.Error(d)
+			}
+		})
+	}
+}
