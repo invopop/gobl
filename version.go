34fc64ffb5e212d3e75345954290797d5225b148
Sam Lown <sam.lown@invopop.com>
2023-02-10 09:03:35 +0000

    Version bump

--- version.go
+++ version.go
@@ -8,7 +8,7 @@ import (
 type Version string
 
 // VERSION is the current version of the GOBL library.
-const VERSION Version = "v0.35.2"
+const VERSION Version = "v0.36.0"
 
 // Semver parses and returns semver
 func (v Version) Semver() *semver.Version {
