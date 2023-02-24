a6330fe228fdf414a63de29cb474b5835aac69c1
Luca Nioi <lucahnioi@gmail.com>
2023-02-24 10:02:56 +0000

    Add italy invoice validation

--- regimes/it/invoice_validator.go
+++ regimes/it/invoice_validator.go
@@ -3,6 +3,7 @@ package it
 import (
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/invopop/gobl/bill"
+	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/org"
 	"github.com/invopop/gobl/tax"
 )
@@ -21,13 +22,14 @@ func validateInvoice(inv *bill.Invoice) error {
 func (v *invoiceValidator) validate() error {
 	inv := v.inv
 	return validation.ValidateStruct(inv,
-		// Currently only supporting invoices and credit notes for Italy
+		// Currently only supporting invoices and credit notes
 		validation.Field(&inv.Type, validation.In(
 			bill.InvoiceTypeNone,
 			bill.InvoiceTypeCreditNote,
 		)),
 		validation.Field(&inv.Supplier, validation.Required, validation.By(v.supplier)),
 		validation.Field(&inv.Customer, validation.Required, validation.By(v.customer)),
+		validation.Field(&inv.Meta, validation.Required, validation.By(v.meta)),
 	)
 }
 
@@ -53,25 +55,103 @@ func (v *invoiceValidator) customer(value interface{}) error {
 	if cus.TaxID == nil {
 		return nil // validation already handled, this prevents panics
 	}
-	// Customers must have a VAT ID (Partita IVA) OR a fiscal code (Codice Fiscale)
+
+	var fiscalCode *org.Identity
+	for _, identity := range cus.Identities {
+		if identity.Type == IdentityTypeFiscalCode {
+			fiscalCode = identity
+			break
+		}
+	}
+
+	// Customers must have either a VAT ID (Partita IVA) or a fiscal code (Codice
+	// Fiscale)
 	return validation.ValidateStruct(cus,
-		validation.Field(&cus.TaxID, validation.When(
-			fiscalCode(cus) != nil,
-			validation.Required,
-			tax.RequireIdentityCode,
-		)),
-		validation.Field(fiscalCode(cus), validation.When(
+		validation.Field(fiscalCode, validation.When(
 			cus.TaxID == nil,
 			validation.Required,
+		)),
+		validation.Field(&cus.TaxID, validation.When(
+			fiscalCode == nil,
+			validation.Required,
+			tax.RequireIdentityCode,
 		)),
 	)
 }
 
-func fiscalCode(party *org.Party) *org.Identity {
-	for _, identity := range party.Identities {
-		if identity.Type == IdentityTypeFiscalCode {
-			return identity
+// Meta data of the invoice must contain valid FatturaPA codes
+func (v *invoiceValidator) meta(value interface{}) error {
+	meta, _ := value.(cbc.Meta)
+	if meta == nil {
+		return nil
+	}
+
+	// Validate Tax System FPA code. Currently only supporting
+	// RF01 "Ordinary" tax system
+	err := validation.Validate(meta[FPACodeTypeTaxSystem],
+		validation.Required,
+		validation.In(FPACodeTaxSystemOrdinary),
+	)
+	if err != nil {
+		return err
+	}
+
+	// Validate Document Type FPA code
+	invoiceType := v.inv.Type
+	codeGroup := InvoiceTypeMap[invoiceType]
+	err = validateMetaFPACode(meta, codeGroup)
+	if err != nil {
+		return err
+	}
+
+	// Validate Payment Method FPA code
+	payMethod := v.inv.Payment.Instructions.Key
+	codeGroup = PaymentMethodMap[payMethod]
+	err = validateMetaFPACode(meta, codeGroup)
+	if err != nil {
+		return err
+	}
+
+	// Validate Tax Scheme FPA code
+	// Currently only reverse charge schemes are supported.
+	for _, scheme := range v.inv.Tax.Schemes {
+		codeGroup = SchemeMap[scheme]
+		err = validateMetaFPACode(meta, codeGroup)
+		if err != nil {
+			return err
 		}
 	}
+
+	// Validate Withholding Tax FPA code
+	// Only retained taxes are considered.
+	for _, cat := range v.inv.Totals.Taxes.Categories {
+		if !taxCategoryFromCode(cat.Code).Retained {
+			break
+		}
+		codeGroup = TaxCategoryMap[cat.Code]
+		err = validateMetaFPACode(meta, codeGroup)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func validateMetaFPACode(meta cbc.Meta, codeGroup *FPACodeGroup) error {
+	if codeGroup == nil {
+		return nil
+	}
+
+	key := codeGroup.Type
+	fpaCodes := codeGroup.Codes
+	codes := make([]interface{}, len(fpaCodes))
+	for i, code := range fpaCodes {
+		codes[i] = code
+	}
+
+	return validation.Validate(meta[key],
+		validation.Required,
+		validation.In(codes...),
+	)
 }
