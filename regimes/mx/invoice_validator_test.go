49b3ded2439a5e0c2faa8bf47fad50d78335ebd9
Luismi Cavallé <luismi@lmcavalle.com>
2023-06-21 12:48:37 +0000

    Refactor to take advantage of `num.Positive`

--- regimes/mx/invoice_validator_test.go
+++ regimes/mx/invoice_validator_test.go
@@ -72,18 +72,18 @@ func TestLineValidation(t *testing.T) {
 	inv := validInvoice()
 
 	inv.Lines[0].Quantity = num.MakeAmount(0, 0)
-	assertValidationError(t, inv, "lines: (0: (quantity: must be positive; total: must be positive.).)")
+	assertValidationError(t, inv, "lines: (0: (quantity: must be greater than 0; total: must be greater than 0.).)")
 
 	inv.Lines[0].Quantity = num.MakeAmount(-1, 0)
-	assertValidationError(t, inv, "lines: (0: (quantity: must be positive; total: must be positive.).)")
+	assertValidationError(t, inv, "lines: (0: (quantity: must be greater than 0; total: must be greater than 0.).)")
 
 	inv = validInvoice()
 
 	inv.Lines[0].Item.Price = num.MakeAmount(0, 0)
-	assertValidationError(t, inv, "lines: (0: (total: must be positive.).)")
+	assertValidationError(t, inv, "lines: (0: (total: must be greater than 0.).)")
 
 	inv.Lines[0].Item.Price = num.MakeAmount(-1, 0)
-	assertValidationError(t, inv, "lines: (0: (total: must be positive.).)")
+	assertValidationError(t, inv, "lines: (0: (total: must be greater than 0.).)")
 
 	inv = validInvoice()
 
