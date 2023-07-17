18f5fe9ecf383d559aebb8e9827fda0509022ea5
Sam Lown <sam.lown@invopop.com>
2023-07-17 11:14:35 +0000

    Setting accuracy to always +2 of quantity

--- version.go
+++ version.go
@@ -8,7 +8,7 @@ import (
 type Version string
 
 // VERSION is the current version of the GOBL library.
-const VERSION Version = "v0.51.4"
+const VERSION Version = "v0.51.5"
 
 // Semver parses and returns semver
 func (v Version) Semver() *semver.Version {
