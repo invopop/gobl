7b4ed4053832394f2d6a0aa3a2b92536f1340e43
Luismi Cavallé <luismi@lmcavalle.com>
2023-09-04 10:34:58 +0000

    Replace dashes with underscores in JSON keys

--- regimes/mx/fuel_complement.go
+++ regimes/mx/fuel_complement.go
@@ -14,7 +14,7 @@ import (
 // information as part of the invoices they issue to their customers.
 type FuelComplement struct {
 	// Customer's e-wallet account number
-	WalletAccountNumber string `json:"wallet-account-number" jsonschema:"title=Wallet Account Number"`
+	WalletAccountNumber string `json:"wallet_account_number" jsonschema:"title=Wallet Account Number"`
 	// List of fuel purchases made with the customer's e-wallets
 	Lines []*FuelLine `json:"lines" jsonschema:"title=Lines"`
 	// Summary of all the purchases totals, including taxes (calculated)
@@ -25,19 +25,19 @@ type FuelComplement struct {
 // the invoice's supplier to the invoice's customer.
 type FuelLine struct {
 	// Identifier of the e-wallet used to make the purchase
-	WalletID cbc.Code `json:"wallet-id" jsonschema:"title=Wallet Identifier"`
+	WalletID cbc.Code `json:"wallet_id" jsonschema:"title=Wallet Identifier"`
 	// Date and time of the purchase
-	PurchaseDateTime civil.DateTime `json:"purchase-date" jsonschema:"title=Purchase Date"`
+	PurchaseDateTime civil.DateTime `json:"purchase_date" jsonschema:"title=Purchase Date"`
 	// Tax ID (RFC) of the purchaser
-	PurchaserTaxID cbc.Code `json:"purchaser-tax-id" jsonschema:"title=Purchaser's Tax ID"`
+	PurchaserTaxID cbc.Code `json:"purchaser_tax_id" jsonschema:"title=Purchaser's Tax ID"`
 	// Code of the service station where the purchase was made
-	ServiceStationCode cbc.Code `json:"service-station-code" jsonschema:"title=Service Station Code"`
+	ServiceStationCode cbc.Code `json:"service_station_code" jsonschema:"title=Service Station Code"`
 	// Amount of fuel units purchased
 	Quantity num.Amount `json:"quantity" jsonschema:"title=Quantity"`
 	// Details about the fuel purchased
 	Item FuelItem `json:"item" jsonschema:"title=Item"`
 	// Identifier of the purchase (folio)
-	PurchaseID cbc.Code `json:"purchase-id" jsonschema:"title=Purchase Identifier"`
+	PurchaseID cbc.Code `json:"purchase_id" jsonschema:"title=Purchase Identifier"`
 	// Result of quantity multiplied by the item's price (calculated)
 	Sum num.Amount `json:"sum" jsonschema:"title=Sum" jsonschema_extras:"calculated=true"`
 	// Map of taxes applied to the purchase
