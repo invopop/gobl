7750c374486befed3c1fd8b6dc0dc26858df4438
Luismi Cavallé <luismi@lmcavalle.com>
2023-11-17 14:13:02 +0000

    Add retained and other MX taxes

--- regimes/mx/tax_categories.go
+++ regimes/mx/tax_categories.go
@@ -8,12 +8,23 @@ import (
 	"github.com/invopop/gobl/tax"
 )
 
-// Tax rates specific to Mexico.
+// Local tax categories
+const (
+	TaxCategoryRVAT  cbc.Code = "RVAT"  // IVA (Retenido)
+	TaxCategoryIEPS  cbc.Code = "IEPS"  // Impuesto Especial sobre Producción y Servicios
+	TaxCategoryRIEPS cbc.Code = "RIEPS" // Impuesto Especial sobre Producción y Servicios (Retenido)
+	TaxCategoryISR   cbc.Code = "ISR"   // Impuesto Sobre la Renta
+)
+
+// Local tax rates
 const (
 	TaxRateExempt cbc.Key = "exempt"
 )
 
 var taxCategories = []*tax.Category{
+	//
+	// IVA
+	//
 	{
 		Code: common.TaxCategoryVAT,
 		Name: i18n.String{
@@ -71,5 +82,78 @@ var taxCategories = []*tax.Category{
 				Exempt: true,
 			},
 		},
+		Map: cbc.CodeMap{KeySATImpuesto: "002"},
+	},
+
+	//
+	// IVA (Retenido)
+	//
+	{
+		Code: TaxCategoryRVAT,
+		Name: i18n.String{
+			i18n.EN: "Retained VAT",
+			i18n.ES: "IVA Retenido",
+		},
+		Title: i18n.String{
+			i18n.EN: "Retained Value Added Tax",
+			i18n.ES: "Impuesto al Valor Agregado Retenido",
+		},
+		Retained: true,
+		Rates:    []*tax.Rate{},
+		Map:      cbc.CodeMap{KeySATImpuesto: "002"},
+	},
+
+	//
+	// IEPS
+	//
+	{
+		Code: TaxCategoryIEPS,
+		Name: i18n.String{
+			i18n.EN: "IEPS",
+			i18n.ES: "IEPS",
+		},
+		Title: i18n.String{
+			i18n.EN: "Special Tax on Production and Services",
+			i18n.ES: "Impuesto Especial sobre Producción y Servicios",
+		},
+		Retained: false,
+		Rates:    []*tax.Rate{},
+		Map:      cbc.CodeMap{KeySATImpuesto: "003"},
+	},
+
+	//
+	// IEPS (Retenido)
+	//
+	{
+		Code: TaxCategoryRIEPS,
+		Name: i18n.String{
+			i18n.EN: "Retained IEPS",
+			i18n.ES: "IEPS Retenido",
+		},
+		Title: i18n.String{
+			i18n.EN: "Retained Special Tax on Production and Services",
+			i18n.ES: "Impuesto Especial sobre Producción y Servicios Retenido",
+		},
+		Retained: true,
+		Rates:    []*tax.Rate{},
+		Map:      cbc.CodeMap{KeySATImpuesto: "003"},
+	},
+
+	//
+	// ISR
+	//
+	{
+		Code: TaxCategoryISR,
+		Name: i18n.String{
+			i18n.EN: "ISR",
+			i18n.ES: "ISR",
+		},
+		Title: i18n.String{
+			i18n.EN: "Income Tax",
+			i18n.ES: "Impuesto Sobre la Renta",
+		},
+		Retained: true,
+		Rates:    []*tax.Rate{},
+		Map:      cbc.CodeMap{KeySATImpuesto: "001"},
 	},
 }
