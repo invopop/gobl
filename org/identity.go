f193b3066d0634dc2e67a1c75f2b76693f70128a
Luismi Cavallé <luismi@lmcavalle.com>
2023-11-22 22:43:20 +0000

    Provide an Identity Key validation rule

--- org/identity.go
+++ org/identity.go
@@ -3,6 +3,7 @@ package org
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/tax"
@@ -55,8 +56,15 @@ func HasIdentityType(typ cbc.Code) validation.Rule {
 	return validateIdentitySet{typ: typ}
 }
 
+// HasIdentityKey provides a validation rule that will determine if at least one
+// of the identities defined includes one with the defined key.
+func HasIdentityKey(key cbc.Key) validation.Rule {
+	return validateIdentitySet{key: key}
+}
+
 type validateIdentitySet struct {
 	typ cbc.Code
+	key cbc.Key
 }
 
 func (v validateIdentitySet) Validate(value interface{}) error {
@@ -65,11 +73,28 @@ func (v validateIdentitySet) Validate(value interface{}) error {
 		return nil
 	}
 	for _, row := range ids {
-		if row.Type == v.typ {
+		if v.matches(row) {
 			return nil
 		}
 	}
-	return fmt.Errorf("missing %s", v.typ)
+
+	return fmt.Errorf("missing %s", v)
+}
+
+func (v validateIdentitySet) matches(row *Identity) bool {
+	return (v.typ == cbc.CodeEmpty || row.Type == v.typ) &&
+		(v.key == cbc.KeyEmpty || row.Key == v.key)
+}
+
+func (v validateIdentitySet) String() string {
+	var parts []string
+	if v.typ != cbc.CodeEmpty {
+		parts = append(parts, fmt.Sprintf("type %s", v.typ))
+	}
+	if v.key != cbc.KeyEmpty {
+		parts = append(parts, fmt.Sprintf("key %s", v.key))
+	}
+	return strings.Join(parts, ", ")
 }
 
 // IdentityForType helps return the identity with a matching type code.
