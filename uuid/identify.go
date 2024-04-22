a67510abb0b319a4121ee76c22669c0019e3074f
Sam Lown <sam.lown@invopop.com>
2024-04-22 14:03:12 +0000

    Adding uuid Identify helper methods

--- uuid/identify.go
+++ uuid/identify.go
@@ -14,3 +14,29 @@ func (d *Identify) GetUUID() UUID {
 func (d *Identify) SetUUID(id UUID) {
 	d.UUID = id
 }
+
+// IdentifyParse will parse the provided string as a UUID in the UUID field,
+// or panic. This is mainly meant to be used in tests.
+func IdentifyParse(s string) Identify {
+	return Identify{UUID: MustParse(s)}
+}
+
+// IdentifyV1 is a helper method to generate a V1 uuid ready to embed.
+func IdentifyV1() Identify {
+	return Identify{UUID: V1()}
+}
+
+// IdentifyV3 is a helper method to generate a V3 uuid ready to embed.
+func IdentifyV3(ns UUID, data []byte) Identify {
+	return Identify{UUID: V3(ns, data)}
+}
+
+// IdentifyV4 is a helper method to generate a V4 uuid ready to embed.
+func IdentifyV4() Identify {
+	return Identify{UUID: V4()}
+}
+
+// IdentifyV5 is a helper method to generate a V5 uuid ready to embed.
+func IdentifyV5(ns UUID, data []byte) Identify {
+	return Identify{UUID: V5(ns, data)}
+}
