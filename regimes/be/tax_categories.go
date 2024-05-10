db21a02713f002803a2f16aa61a7f82be34b83fd
Sam Lown <sam.lown@invopop.com>
2024-05-10 09:28:26 +0000

    Adding basic BE tax regime

---
+++ regimes/be/tax_categories.go
@@ -0,0 +1,90 @@
+package be
+
+import (
+	"github.com/invopop/gobl/cal"
+	"github.com/invopop/gobl/i18n"
+	"github.com/invopop/gobl/num"
+	"github.com/invopop/gobl/tax"
+)
+
+var taxCategories = []*tax.Category{
+	//
+	// VAT
+	//
+	{
+		Code: tax.CategoryVAT,
+		Name: i18n.String{
+			i18n.EN: "VAT",
+		},
+		Title: i18n.String{
+			i18n.EN: "Value Added Tax",
+		},
+		Sources: []*tax.Source{
+			{
+				Title: i18n.String{
+					i18n.EN: "VAT rates | FPS Finance",
+				},
+				URL: "https://finance.belgium.be/en/enterprises/vat/vat-obligation/rates-and-calculation/vat-rates#q1",
+			},
+		},
+		Retained: false,
+		Rates: []*tax.Rate{
+			{
+				Key: tax.RateStandard,
+				Name: i18n.String{
+					i18n.EN: "Standard Rate",
+					i18n.NL: "Standaardtarief",
+					i18n.FR: "Taux standard",
+					i18n.DE: "Standardsatz",
+				},
+				Values: []*tax.RateValue{
+					{
+						Since:   cal.NewDate(2011, 1, 4),
+						Percent: num.MakePercentage(210, 3),
+					},
+				},
+			},
+			{
+				Key: tax.RateIntermediate,
+				Name: i18n.String{
+					i18n.EN: "Intermediate Rate",
+					i18n.NL: "Tussentarief",
+					i18n.FR: "Taux intermédiaire",
+					i18n.DE: "Mittlerer Satz",
+				},
+				Values: []*tax.RateValue{
+					{
+						Since:   cal.NewDate(2011, 1, 1),
+						Percent: num.MakePercentage(120, 3),
+					},
+				},
+			},
+			{
+				Key: tax.RateReduced,
+				Name: i18n.String{
+					i18n.EN: "Reduced Rate",
+					i18n.NL: "Verlaagd Tarief",
+					i18n.FR: "Taux réduit",
+					i18n.DE: "Ermäßigter Satz",
+				},
+				Values: []*tax.RateValue{
+					{
+						Since:   cal.NewDate(2011, 1, 4),
+						Percent: num.MakePercentage(60, 3),
+					},
+				},
+			},
+			{
+				Key: tax.RateZero,
+				Name: i18n.String{
+					i18n.EN: "Zero Rate",
+				},
+				Values: []*tax.RateValue{
+					{
+						Percent: num.MakePercentage(0, 3),
+					},
+				},
+			},
+		},
+	},
+}
