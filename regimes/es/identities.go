4b2640fe9889b1d981411fe90a087d606a543e35
Sam Lown <sam.lown@invopop.com>
2024-09-24 08:08:04 +0000

    Defining some generic identities

--- regimes/es/identities.go
+++ regimes/es/identities.go
@@ -3,41 +3,33 @@ package es
 import (
 	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/i18n"
-)
-
-// Spain recognises a certain set of identity types specifically that may be mapped
-// to local keys.
-const (
-	IdentityKeyPassport  cbc.Key = "es-passport"
-	IdentityKeyForeignID cbc.Key = "es-foreign-id"
-	IdentityKeyResident  cbc.Key = "es-resident"
-	IdentityKeyOther     cbc.Key = "es-other"
+	"github.com/invopop/gobl/org"
 )
 
 var identityKeyDefinitions = []*cbc.KeyDefinition{
 	{
-		Key: IdentityKeyPassport,
+		Key: org.IdentityKeyPassport,
 		Name: i18n.String{
 			i18n.EN: "Passport",
 			i18n.ES: "Pasaporte",
 		},
 	},
 	{
-		Key: IdentityKeyForeignID,
+		Key: org.IdentityKeyForeign,
 		Name: i18n.String{
 			i18n.EN: "National ID Card or similar from a foreign country",
 			i18n.ES: "Documento oficial de identificación expedido por el país o territorio de residencia",
 		},
 	},
 	{
-		Key: IdentityKeyResident,
+		Key: org.IdentityKeyResident,
 		Name: i18n.String{
 			i18n.EN: "Residential permit",
 			i18n.ES: "Certificado de residencia",
 		},
 	},
 	{
-		Key: IdentityKeyOther,
+		Key: org.IdentityKeyOther,
 		Name: i18n.String{
 			i18n.EN: "An other type of source not listed",
 			i18n.ES: "Otro documento probatorio",
