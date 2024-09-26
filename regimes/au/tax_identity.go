6bfce32c7dbd980e0337cf053e3509e26c4f5436
Antonio Pardo de Santayana <201903588@alu.comillas.edu>
2024-09-26 11:08:47 +0200

    Added ACN support

--- regimes/au/tax_identity.go
+++ regimes/au/tax_identity.go
@@ -13,6 +13,9 @@ import (
 // Weights for ABN checksum
 var taxWeightTableABN = [11]int{10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19}
 
+// Weights for ACN checksum
+var taxWeightTableACN = [8]int{8, 7, 6, 5, 4, 3, 2, 1}
+
 func validateTaxIdentity(tID *tax.Identity) error {
 	return validation.ValidateStruct(tID,
 		validation.Field(&tID.Code, validation.By(validateTaxCode)),
@@ -26,21 +29,24 @@ func validateTaxCode(value interface{}) error {
 	}
 	val := code.String()
 	reABN := regexp.MustCompile(`^\d{11}$`)
+	reACN := regexp.MustCompile(`^\d{9}$`)
 
 	switch {
 	case reABN.MatchString(val):
 		return businessNumberCheck(val)
+	case reACN.MatchString(val):
+		return companyNumberCheck(val)
 	default:
 		return errors.New("invalid format")
 	}
 }
 
+// Source: https://abr.business.gov.au/Help/AbnFormat
 func businessNumberCheck(val string) error {
 	if z, _ := strconv.Atoi(val); z == 0 {
 		return errors.New("zeros")
 	}
 
-	// Source: https://abr.business.gov.au/Help/AbnFormat
 	firstDigit, err := strconv.Atoi(string(val[0]))
 	if err != nil {
 		return errors.New("invalid format")
@@ -56,6 +62,30 @@ func businessNumberCheck(val string) error {
 		sum += digit * taxWeightTableABN[i]
 	}
 	if sum%89 == 0 {
+		return nil
+	}
+	return errors.New("checksum mismatch")
+}
+
+// Source: https://asic.gov.au/for-business/registering-a-company/steps-to-register-a-company/australian-company-numbers/australian-company-number-digit-check
+func companyNumberCheck(val string) error {
+	if z, _ := strconv.Atoi(val); z == 0 {
+		return errors.New("zeros")
+	}
+	checkDigit, err := strconv.Atoi(string(val[8]))
+	if err != nil {
+		return errors.New("invalid format")
+	}
+	sum := 0
+	for i := 0; i < 8; i++ {
+		digit, err := strconv.Atoi(string(val[i]))
+		if err != nil {
+			return errors.New("invalid format")
+		}
+		sum += digit * taxWeightTableACN[i]
+	}
+	remainder := sum % 10
+	if (10-remainder == checkDigit) || (remainder == 0 && checkDigit == 0) {
 		return nil
 	}
 	return errors.New("checksum mismatch")
