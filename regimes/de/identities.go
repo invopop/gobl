7652b50c651a2c4e6cfd9a229d1dcd7b6ce39c20
Antonio Pardo de Santayana <201903588@alu.comillas.edu>
2024-10-01 12:17:33 +0200

    Added Identifikationsnummer support

--- regimes/de/identities.go
+++ regimes/de/identities.go
@@ -3,6 +3,7 @@ package de
 import (
 	"errors"
 	"regexp"
+	"strconv"
 
 	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/i18n"
@@ -38,7 +39,7 @@ var identityKeyDefinitions = []*cbc.KeyDefinition{
 
 // Normalize will attempt to clean the object passed to it.
 func normalizeIdentity(id *org.Identity) {
-	if id == nil || id.Key != IdentityKeyTaxNumber {
+	if id == nil || (id.Key != IdentityKeyTaxNumber && id.Key != IdentityKeyTaxID) {
 		return
 	}
 	code := id.Code.String()
@@ -48,20 +49,32 @@ func normalizeIdentity(id *org.Identity) {
 
 // Validate checks the document type and determines if it can be validated.
 func validateIdentity(id *org.Identity) error {
-	if id == nil || id.Key != IdentityKeyTaxNumber {
+	if id == nil || (id.Key != IdentityKeyTaxNumber && id.Key != IdentityKeyTaxID) {
 		return nil
 	}
-	return validation.ValidateStruct(id,
-		validation.Field(&id.Code,
-			validation.Required,
-			validation.By(validateIDNumber),
-			validation.Skip,
-		),
-	)
+	if id.Key == IdentityKeyTaxNumber {
+		return validation.ValidateStruct(id,
+			validation.Field(&id.Code,
+				validation.Required,
+				validation.By(validateTaxNumber),
+				validation.Skip,
+			),
+		)
+	}
+	if id.Key == IdentityKeyTaxID {
+		return validation.ValidateStruct(id,
+			validation.Field(&id.Code,
+				validation.Required,
+				validation.By(validateTaxID),
+				validation.Skip,
+			),
+		)
+	}
+	return nil
 }
 
 // ValidateTaxNumber checks the document type and determines if it can be validated.
-func validateIDNumber(value interface{}) error {
+func validateTaxNumber(value interface{}) error {
 	val, ok := value.(cbc.Code)
 	if !ok || val == cbc.CodeEmpty {
 		return nil
@@ -80,5 +93,83 @@ func validateIDNumber(value interface{}) error {
 	}
 
 	return nil
+}
 
+func validateTaxID(value interface{}) error {
+	val, ok := value.(cbc.Code)
+	if !ok || val == cbc.CodeEmpty {
+		return nil
+	}
+	code := val.String()
+	if match, _ := regexp.MatchString(`^\d+$`, code); !match {
+		return errors.New("invalid format: tax number should only contain digits")
+	}
+
+	var codeNumber string
+	switch len(code) {
+	case 10, 11:
+		codeNumber = code
+	case 12, 13:
+		codeNumber = code[2:]
+	default:
+		return errors.New("invalid length")
+	}
+	// Check if the first digit is not 0
+	if codeNumber[0] == '0' {
+		return errors.New("invalid format: first digit cannot be 0")
+	}
+	// Check digit occurrence rule for the first ten digits: https://de.wikipedia.org/wiki/Steuerliche_Identifikationsnummer
+	digitCount := make(map[rune]int)
+	for _, digit := range codeNumber[:10] {
+		digitCount[digit]++
+	}
+
+	twiceOrThrice := false
+	zeroOccurrence := 0
+	singleOccurrence := 0
+
+	for _, count := range digitCount {
+		switch count {
+		case 2, 3:
+			if twiceOrThrice {
+				return errors.New("invalid format: more than one digit appears twice or thrice")
+			}
+			twiceOrThrice = true
+		case 0:
+			zeroOccurrence++
+		case 1:
+			singleOccurrence++
+		default:
+			return errors.New("invalid format: a digit appears more than three times")
+		}
+	}
+
+	if !twiceOrThrice || zeroOccurrence > 2 || singleOccurrence < 7 || singleOccurrence > 8 {
+		return errors.New("invalid format: digit occurrence rule not satisfied")
+	}
+
+	// Extract digits and check digit ISO/IEC 7064 MOD 11,10
+	digits := make([]int, len(codeNumber)-1)
+	for i := 0; i < len(codeNumber)-1; i++ {
+		digit, _ := strconv.Atoi(string(codeNumber[i]))
+		digits[i] = digit
+	}
+	RealCheckDigit, _ := strconv.Atoi(string(codeNumber[len(codeNumber)-1]))
+	prod := 10
+	for _, digit := range digits {
+		sum := (digit + prod) % 10
+		if sum == 0 {
+			sum = 10
+		}
+		prod = (sum * 2) % 11
+	}
+	checkDigit := 11 - prod
+	if checkDigit == 10 {
+		checkDigit = 0
+	}
+	if RealCheckDigit == checkDigit {
+		return nil
+	}
+
+	return errors.New("checksum mismatch")
 }
--- regimes/de/identities_test.go
+++ regimes/de/identities_test.go
@@ -21,7 +21,7 @@ func TestIdentityNormalization(t *testing.T) {
 	assert.Equal(t, "12345678901", id.Code.String())
 }
 
-func TestIdentityValidation(t *testing.T) {
+func TestTaxNumberValidation(t *testing.T) {
 	tests := []struct {
 		name string
 		code cbc.Code
@@ -42,7 +42,48 @@ func TestIdentityValidation(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			id := &org.Identity{Key: de.IdentityKeyTaxNumber, Code: tt.code}
+			err := de.Validate(id)
+			if tt.err == "" {
+				assert.NoError(t, err)
+			} else {
+				if assert.Error(t, err) {
+					assert.Contains(t, err.Error(), tt.err)
+				}
+			}
+		})
+	}
+}
 
+func TestTaxIDValidation(t *testing.T) {
+	tests := []struct {
+		name string
+		code cbc.Code
+		err  string
+	}{
+		{name: "valid 11 digits 1", code: "86095742719"},
+		{name: "valid 11 digits 2", code: "65929970489"},
+		{name: "valid 11 digits 3", code: "57549285017"},
+		{name: "valid 13 digits", code: "9947036892816"},
+
+		// Invalid formats
+		{name: "too short", code: "12345678", err: "invalid length"},
+		{name: "too long", code: "12345678901234", err: "invalid length"},
+		{name: "non-numeric", code: "123ABC78901", err: "should only contain digits"},
+		{name: "starts with zero", code: "01234567890", err: "first digit cannot be 0"},
+
+		// Invalid digit occurrence
+		{name: "no digit twice", code: "12345678901", err: "digit occurrence rule not satisfied"},
+		{name: "two digits twice", code: "11223344556", err: "more than one digit appears twice"},
+		{name: "digit four times", code: "11112222333", err: "a digit appears more than three times"},
+
+		// Checksum tests
+		{name: "invalid checksum 1", code: "12345678910", err: "checksum mismatch"},
+		{name: "invalid checksum 2", code: "98765432130", err: "checksum mismatch"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			id := &org.Identity{Key: de.IdentityKeyTaxID, Code: tt.code}
 			err := de.Validate(id)
 			if tt.err == "" {
 				assert.NoError(t, err)
