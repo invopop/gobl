860d396d6500102f33547568773f9a1ed642c473
Sam Lown <sam.lown@invopop.com>
2024-10-11 07:22:00 +0000

    Fixing linter issues

--- addons/it/sdi/identities.go
+++ addons/it/sdi/identities.go
@@ -3,8 +3,8 @@ package sdi
 import "github.com/invopop/gobl/cbc"
 
 const (
-	// Code managed by the CIPE (Interministerial Committee for Economic Planning) which characterises every public investment project (Individual Project Code)
+	// IdentityTypeCUP defines code managed by the CIPE (Interministerial Committee for Economic Planning) which characterises every public investment project (Individual Project Code)
 	IdentityTypeCUP cbc.Code = "CUP"
-	// Tender procedure identification code
+	// IdentityTypeCIG defines tender procedure identification code.
 	IdentityTypeCIG cbc.Code = "CIG"
 )
