098c3510bf4f4c8c0f86f19b910ffa1a35a1dfd4
apardods <antonio.pardo@invopop.com>
2024-11-08 11:32:54 +0000

    Add Invoice Tests

---
+++ addons/es/verifactu/invoice_test.go
@@ -0,0 +1,102 @@
+package verifactu_test
+
+import (
+	"testing"
+
+	"github.com/invopop/gobl/addons/es/verifactu"
+	"github.com/invopop/gobl/bill"
+	"github.com/invopop/gobl/cbc"
+	"github.com/invopop/gobl/num"
+	"github.com/invopop/gobl/org"
+	"github.com/invopop/gobl/tax"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInvoiceValidation(t *testing.T) {
+	t.Run("standard invoice", func(t *testing.T) {
+		inv := testInvoiceStandard(t)
+		require.NoError(t, inv.Calculate())
+		require.NoError(t, inv.Validate())
+	})
+	t.Run("missing customer tax ID", func(t *testing.T) {
+		inv := testInvoiceStandard(t)
+		inv.Customer.TaxID = nil
+		require.NoError(t, inv.Calculate())
+		err := inv.Validate()
+		assert.ErrorContains(t, err, "customer: (tax_id: cannot be blank.)")
+	})
+
+	t.Run("with exemption reason", func(t *testing.T) {
+		inv := testInvoiceStandard(t)
+		inv.Lines[0].Taxes[0].Ext = nil
+		assertValidationError(t, inv, "es-verifactu-exemption: required")
+	})
+
+	t.Run("without series", func(t *testing.T) {
+		inv := testInvoiceStandard(t)
+		inv.Series = ""
+		assertValidationError(t, inv, "series: cannot be blank")
+	})
+
+	t.Run("without notes", func(t *testing.T) {
+		inv := testInvoiceStandard(t)
+		inv.Notes = nil
+		assertValidationError(t, inv, "notes: with key 'general' missing")
+	})
+}
+
+func assertValidationError(t *testing.T, inv *bill.Invoice, expected string) {
+	require.NoError(t, inv.Calculate())
+	err := inv.Validate()
+	require.ErrorContains(t, err, expected)
+}
+
+func testInvoiceStandard(t *testing.T) *bill.Invoice {
+	t.Helper()
+	return &bill.Invoice{
+		Addons: tax.WithAddons(verifactu.V1),
+		Series: "ABC",
+		Code:   "123",
+		Supplier: &org.Party{
+			Name: "Test Supplier",
+			TaxID: &tax.Identity{
+				Country: "ES",
+				Code:    "B98602642",
+			},
+		},
+		Customer: &org.Party{
+			Name: "Test Customer",
+			TaxID: &tax.Identity{
+				Country: "NL",
+				Code:    "000099995B57",
+			},
+		},
+		Lines: []*bill.Line{
+			{
+				Quantity: num.MakeAmount(1, 0),
+				Item: &org.Item{
+					Name:  "bogus",
+					Price: num.MakeAmount(10000, 2),
+					Unit:  org.UnitPackage,
+				},
+				Taxes: tax.Set{
+					{
+						Category: "VAT",
+						Rate:     "exempt",
+						Ext: tax.Extensions{
+							verifactu.ExtKeyExemption: "E1",
+						},
+					},
+				},
+			},
+		},
+		Notes: []*cbc.Note{
+			{
+				Key:  cbc.NoteKeyGeneral,
+				Text: "This is a test invoice",
+			},
+		},
+	}
+}
