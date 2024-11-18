c8c21d9050a539bacf45ad5ec39d57eec5c3f235
Luismi Cavallé <luismi@lmcavalle.com>
2024-11-18 06:07:54 +0000

    Add validations for BR suppliers

--- addons/br/nfse/invoices.go
+++ addons/br/nfse/invoices.go
@@ -1,8 +1,23 @@
 package nfse
 
 import (
+	"regexp"
+
 	"github.com/invopop/gobl/bill"
+	"github.com/invopop/gobl/cbc"
+	"github.com/invopop/gobl/org"
+	"github.com/invopop/gobl/tax"
 	"github.com/invopop/validation"
+)
+
+var (
+	validStates = []cbc.Code{
+		"AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO",
+		"MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR",
+		"RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO",
+	}
+
+	validAddressCode = regexp.MustCompile(`^(?:\D*\d){8}\D*$`)
 )
 
 func validateInvoice(inv *bill.Invoice) error {
@@ -11,6 +26,10 @@ func validateInvoice(inv *bill.Invoice) error {
 	}
 
 	return validation.ValidateStruct(inv,
+		validation.Field(&inv.Supplier,
+			validation.By(validateSupplier),
+			validation.Skip,
+		),
 		validation.Field(&inv.Charges,
 			validation.Empty.Error("not supported by nfse"),
 			validation.Skip,
@@ -18,6 +37,58 @@ func validateInvoice(inv *bill.Invoice) error {
 		validation.Field(&inv.Discounts,
 			validation.Empty.Error("not supported by nfse"),
 			validation.Skip,
+		),
+	)
+}
+
+func validateSupplier(value interface{}) error {
+	obj, _ := value.(*org.Party)
+	if obj == nil {
+		return nil
+	}
+
+	return validation.ValidateStruct(obj,
+		validation.Field(&obj.TaxID,
+			validation.Required,
+			tax.RequireIdentityCode,
+		),
+		validation.Field(&obj.Identities,
+			org.RequireIdentityKey(IdentityKeyMunicipalReg),
+		),
+		validation.Field(&obj.Name, validation.Required),
+		validation.Field(&obj.Addresses,
+			validation.Required,
+			validation.Each(
+				validation.Required,
+				validation.By(validateSupplierAddress),
+			),
+		),
+		validation.Field(&obj.Ext,
+			tax.ExtensionsRequires(
+				ExtKeySimplesNacional,
+				ExtKeyMunicipality,
+			),
+		),
+	)
+}
+
+func validateSupplierAddress(value interface{}) error {
+	obj, _ := value.(*org.Address)
+	if obj == nil {
+		return nil
+	}
+
+	return validation.ValidateStruct(obj,
+		validation.Field(&obj.Street, validation.Required),
+		validation.Field(&obj.Number, validation.Required),
+		validation.Field(&obj.Locality, validation.Required),
+		validation.Field(&obj.State,
+			validation.Required,
+			validation.In(validStates...),
+		),
+		validation.Field(&obj.Code,
+			validation.Required,
+			validation.Match(validAddressCode),
 		),
 	)
 }
--- addons/br/nfse/invoices_test.go
+++ addons/br/nfse/invoices_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/invopop/gobl/addons/br/nfse"
 	"github.com/invopop/gobl/bill"
 	"github.com/invopop/gobl/num"
+	"github.com/invopop/gobl/org"
 	"github.com/invopop/gobl/tax"
 	"github.com/stretchr/testify/assert"
 )
@@ -61,4 +62,106 @@ func TestInvoicesValidation(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestSuppliersValidation(t *testing.T) {
+	addon := tax.AddonForKey(nfse.V1)
+
+	t.Run("validates supplier", func(t *testing.T) {
+		sup := new(org.Party)
+		inv := &bill.Invoice{
+			Supplier: sup,
+		}
+		err := addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), "name: cannot be blank")
+		}
+
+		sup.Name = "Test"
+		err = addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.NotContains(t, err.Error(), "name: cannot be blank")
+		}
+	})
+
+	t.Run("validates tax ID", func(t *testing.T) {
+		sup := new(org.Party)
+		inv := &bill.Invoice{
+			Supplier: sup,
+		}
+		err := addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), "tax_id: cannot be blank")
+		}
+
+		sup.TaxID = new(tax.Identity)
+		err = addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.NotContains(t, err.Error(), "tax_id: cannot be blank")
+			assert.Contains(t, err.Error(), "tax_id: (code: cannot be blank")
+		}
+
+		sup.TaxID.Code = "123"
+		err = addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.NotContains(t, err.Error(), "tax_id: (code: cannot be blank")
+		}
+	})
+
+	t.Run("validates identities", func(t *testing.T) {
+		sup := new(org.Party)
+		inv := &bill.Invoice{
+			Supplier: sup,
+		}
+		err := addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), "identities: missing key br-nfse-municipal-reg;")
+		}
+
+		sup.Identities = append(sup.Identities, &org.Identity{
+			Key:  nfse.IdentityKeyMunicipalReg,
+			Code: "12345678",
+		})
+		err = addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.NotContains(t, err.Error(), "identities: missing key br-nfse-municipal-reg;")
+		}
+	})
+
+	t.Run("validates addresses", func(t *testing.T) {
+		sup := new(org.Party)
+		inv := &bill.Invoice{
+			Supplier: sup,
+		}
+		err := addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), "addresses: cannot be blank")
+		}
+
+		sup.Addresses = []*org.Address{nil}
+		err = addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.NotContains(t, err.Error(), "addresses: cannot be blank")
+			assert.Contains(t, err.Error(), "addresses: (0: cannot be blank.)")
+		}
+
+		sup.Addresses[0] = new(org.Address)
+		err = addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.NotContains(t, err.Error(), "addresses: (0: cannot be blank.)")
+			assert.Contains(t, err.Error(), "addresses: (0: (code: cannot be blank; locality: cannot be blank; num: cannot be blank; state: cannot be blank; street: cannot be blank.).)")
+		}
+
+		sup.Addresses[0] = &org.Address{
+			Code:     "12345678",
+			Locality: "Test",
+			Number:   "123",
+			State:    "RJ",
+			Street:   "Test",
+		}
+		err = addon.Validator(inv)
+		if assert.Error(t, err) {
+			assert.NotContains(t, err.Error(), "addresses: (0:")
+		}
+	})
 }
