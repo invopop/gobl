ce4a27505e26a21bfc02fca42ac4b2e85c8e05e8
Sam Lown <sam.lown@invopop.com>
2025-01-24 22:07:53 +0000

    WIP: started on support for Factur-X

---
+++ addons/fr/facturx/invoices.go
@@ -0,0 +1,54 @@
+package facturx
+
+import (
+	"github.com/invopop/gobl/bill"
+	"github.com/invopop/gobl/catalogues/untdid"
+	"github.com/invopop/gobl/cbc"
+	"github.com/invopop/gobl/tax"
+	"github.com/invopop/validation"
+)
+
+// BR-DE-17 - restricted subset of UNTDID document type codes
+var validInvoiceUNTDIDDocumentTypeValues = []cbc.Code{
+	"326", // Partial
+	"380", // Commercial
+	"384", // Corrected
+	"389", // Self-billed
+	"381", // Credit note
+	"875", // Partial construction invoice
+	"876", // Partial Final construction invoice
+	"877", // Final construction invoice
+}
+
+func validateInvoice(inv *bill.Invoice) error {
+	return validation.ValidateStruct(inv,
+		// BR-DE-17
+		validation.Field(&inv.Tax,
+			validation.By(validateInvoiceTax),
+			validation.Skip,
+		),
+		validation.Field(&inv.Preceding,
+			validation.When(
+				inv.Type.In(
+					bill.InvoiceTypeCorrective,
+					bill.InvoiceTypeCreditNote,
+				),
+				validation.Required,
+			),
+			validation.Skip,
+		),
+	)
+}
+
+func validateInvoiceTax(value any) error {
+	tx, ok := value.(*bill.Tax)
+	if !ok || tx == nil {
+		return nil
+	}
+	return validation.ValidateStruct(tx,
+		validation.Field(&tx.Ext,
+			tax.ExtensionsHasCodes(untdid.ExtKeyTaxCategory, validInvoiceUNTDIDDocumentTypeValues...),
+			validation.Skip,
+		),
+	)
+}
