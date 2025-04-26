657557da368d435cf27ffe512039aeba67575e3f
Bosco Domingo <boscodomingob@gmail.com>
2025-04-26 18:11:17 +0200

    feat(sweden): add skeleton

---
+++ regimes/se/se.go
@@ -0,0 +1,37 @@
+// Package se provides a regime definition for Sweden.
+package se
+
+import (
+	"github.com/invopop/gobl/currency"
+	"github.com/invopop/gobl/i18n"
+	"github.com/invopop/gobl/regimes/common"
+	"github.com/invopop/gobl/tax"
+)
+
+func init() {
+	tax.RegisterRegimeDef(New())
+}
+
+// New instantiates a new Swedish regime.
+func New() *tax.RegimeDef {
+	return &tax.RegimeDef{
+		Country:   "SE",
+		Currency:  currency.SEK,
+		TaxScheme: tax.CategoryVAT,
+		Name: i18n.String{
+			i18n.EN: "Sweden",
+			i18n.SE: "Sverige",
+		},
+		TimeZone: "Europe/Stockholm",
+		Tags: []*tax.TagSet{
+			common.InvoiceTags(),
+		},
+		// Identities: identityKeyDefinitions, // identities.go
+		// Categories: taxCategories,          // tax_categories.go
+		Scenarios: []*tax.ScenarioSet{
+			common.InvoiceScenarios(),
+		},
+		// Validator:  Validate,
+		// Normalizer: Normalize,
+	}
+}
