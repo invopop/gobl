08a24572bec0d3fcd99e5e312e7a3a95996a75d3
Bosco Domingo <boscodomingob@gmail.com>
2025-05-02 15:10:52 +0200

    feat(sweden): improve support for simplified invoices

--- regimes/se/invoices.go
+++ regimes/se/invoices.go
@@ -11,6 +11,27 @@ import (
 // invoiceValidator adds validation checks to invoices which are relevant
 // for the region.
 func validateInvoice(inv *bill.Invoice) error {
+	if inv.Tags.HasTags(tax.TagSimplified) {
+		// Simplified invoices only require a supplier tax ID.
+		return validation.ValidateStruct(inv,
+			validation.Field(&inv.Customer,
+				validation.When(
+					inv.Customer != nil,
+					validation.Empty,
+				),
+				validation.Nil,
+				validation.Skip,
+			),
+			validation.Field(&inv.Supplier,
+				validation.Required,
+				validation.By(validateSupplier),
+				validation.By(validateSupplierSimplifiedInvoice),
+				validation.Skip,
+			),
+		)
+	}
+
+	// Standard invoices require a supplier and customer.
 	return validation.ValidateStruct(inv,
 		validation.Field(&inv.Supplier,
 			validation.Required,
@@ -21,18 +42,19 @@ func validateInvoice(inv *bill.Invoice) error {
 		validation.Field(&inv.Customer,
 			validation.Required,
 			validation.By(validateOrgParty),
-			validation.By(validateCustomer),
 			validation.Skip,
 		),
 	)
 }
 
+// validateOrgParty holds the common checks for both the supplier and customer.
 func validateOrgParty(value any) error {
 	party, ok := value.(*org.Party)
 	if !ok || party == nil {
 		return nil
 	}
 
+	// Name and addresses are always required.
 	return validation.ValidateStruct(party,
 		validation.Field(&party.Name,
 			validation.Required,
@@ -42,11 +64,31 @@ func validateOrgParty(value any) error {
 			validation.Required,
 			validation.Skip,
 		),
+		validation.Field(&party.Identities,
+			// If the party is registered in Sweden for tax purposes,
+			// then its identities must be one of the allowed types.
+			validation.When(
+				party.TaxID != nil && party.TaxID.Country == l10n.TaxCountryCode(l10n.SE),
+				validation.Each(
+					validation.By(func(value any) error {
+						id, ok := value.(*org.Identity)
+						if !ok || id == nil {
+							return nil
+						}
+						if !id.Type.In(IdentityTypeOrgNr, IdentityTypePersonNr, IdentityTypeCoordinationNr) {
+							return validation.NewError("type", "must be one of: SE-ON, SE-PN, SE-CN")
+						}
+						return nil
+					}),
+				),
+			),
+			validation.Skip,
+		),
 	)
 }
 
-// validateSupplier checks the supplier's tax ID and organization number requirements.
-// The supplier's VAT number and ID Number are always required.
+// validateSupplier checks the supplier's tax ID requirements.
+// The supplier's VAT number is always required.
 func validateSupplier(value any) error {
 	party, ok := value.(*org.Party)
 	if !ok || party == nil {
@@ -58,7 +100,8 @@ func validateSupplier(value any) error {
 		// and must have the correct format.
 		validation.Field(&party.TaxID,
 			validation.Required,
-			validation.By(func(value interface{}) error {
+			tax.RequireIdentityCode,
+			validation.By(func(value any) error {
 				tID, ok := value.(*tax.Identity)
 				if !ok || tID == nil {
 					return nil
@@ -67,35 +110,23 @@ func validateSupplier(value any) error {
 			}),
 			validation.Skip,
 		),
-		validation.Field(&party.Identities,
-			validation.Required,
-			validation.In(
-				org.RequireIdentityType(IdentityTypeOrgNr),
-				org.RequireIdentityType(IdentityTypePersonNr),
-				org.RequireIdentityType(IdentityTypeCoordinationNr),
-			),
-		),
 	)
 }
 
-// validateCustomer checks the customer's tax ID and organization number requirements.
-// However, the customer may not include any identities, just a name and address.
-func validateCustomer(value any) error {
+func validateSupplierSimplifiedInvoice(value any) error {
 	party, ok := value.(*org.Party)
 	if !ok || party == nil {
 		return nil
 	}
 
 	return validation.ValidateStruct(party,
-		validation.Field(&party.Identities,
-			validation.When(
-				party.TaxID != nil && party.TaxID.Country == l10n.TaxCountryCode(l10n.SE),
-				validation.In(
-					org.RequireIdentityType(IdentityTypeOrgNr),
-					org.RequireIdentityType(IdentityTypePersonNr),
-					org.RequireIdentityType(IdentityTypeCoordinationNr),
-				),
-			),
+		validation.Field(&party.Name,
+			validation.NilOrNotEmpty,
+			validation.Skip,
+		),
+		validation.Field(&party.Addresses,
+			validation.NilOrNotEmpty,
+			validation.Skip,
 		),
 	)
 }
