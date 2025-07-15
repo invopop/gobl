958e3c0e5d8a530a7d70bdfaff52fee737482754
Sam Lown <sam.lown@invopop.com>
2025-07-15 15:51:28 +0000

    Quick release with EU validation and improved Verifactu copy

--- version.go
+++ version.go
@@ -8,7 +8,7 @@ import (
 type Version string
 
 // VERSION is the current version of the GOBL library.
-const VERSION Version = "v0.220.2"
+const VERSION Version = "v0.220.3"
 
 // Semver parses and returns semver
 func (v Version) Semver() *semver.Version {
