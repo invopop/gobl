aaefc85fdbc332420884a219323b2d196b4d8f1e
Sam Lown <sam.lown@invopop.com>
2025-10-29 09:35:36 +0000

    Lint and test fixes

--- regimes/ie/ie.go
+++ regimes/ie/ie.go
@@ -6,7 +6,6 @@ import (
 	"github.com/invopop/gobl/currency"
 	"github.com/invopop/gobl/i18n"
 	"github.com/invopop/gobl/l10n"
-	"github.com/invopop/gobl/org"
 	"github.com/invopop/gobl/tax"
 )
 
@@ -48,12 +47,5 @@ func Normalize(doc any) {
 	switch obj := doc.(type) {
 	case *tax.Identity:
 		tax.NormalizeIdentity(obj)
-	case *org.Identity:
-		normalizeOrgIdentity(obj)
 	}
-}
-
-// normalizeOrgIdentity performs normalization on org identities.
-func normalizeOrgIdentity(id *org.Identity) {
-	// Placeholder for org identity normalization if needed in the future
 }
