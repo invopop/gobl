7a3e441707b4c92d869118c5c94f6df7e2e47a58
pmenendz <pabloj.menendez@gmail.com>
2026-01-24 11:33:45 +0000

    Handle the NRW format for Steuernummer

--- regimes/de/identities.go
+++ regimes/de/identities.go
@@ -3,6 +3,7 @@ package de
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/i18n"
@@ -18,7 +19,8 @@ const (
 	IdentityKeyTaxNumber cbc.Key = "de-tax-number"
 )
 
-var taxNumberRegexPattern = regexp.MustCompile(`^\d{2,3}/\d{3}/\d{5}$`)
+// Valid formats: 2/3/5 (10 digits), 3/3/5 (11 digits standard), or 3/4/4 (11 digits NRW)
+var taxNumberRegexPattern = regexp.MustCompile(`^(\d{2}/\d{3}/\d{5}|\d{3}/\d{3}/\d{5}|\d{3}/\d{4}/\d{4})$`)
 
 var identityDefinitions = []*cbc.Definition{
 	{
@@ -35,12 +37,33 @@ func normalizeTaxNumber(id *org.Identity) {
 	if id == nil || id.Key != IdentityKeyTaxNumber {
 		return
 	}
+
+	// Check if input already has the NRW format (3/4/4)
+	// If so, preserve it as-is
+	original := id.Code.String()
+	if strings.Count(original, "/") == 2 {
+		parts := strings.Split(original, "/")
+		if len(parts) == 3 {
+			// Extract only digits from each part
+			p1 := cbc.NormalizeNumericalCode(cbc.Code(parts[0])).String()
+			p2 := cbc.NormalizeNumericalCode(cbc.Code(parts[1])).String()
+			p3 := cbc.NormalizeNumericalCode(cbc.Code(parts[2])).String()
+
+			// Check if it matches NRW format (3/4/4)
+			if len(p1) == 3 && len(p2) == 4 && len(p3) == 4 {
+				id.Code = cbc.Code(fmt.Sprintf("%s/%s/%s", p1, p2, p3))
+				return
+			}
+		}
+	}
+
+	// Otherwise, normalize to standard format
 	code := cbc.NormalizeNumericalCode(id.Code).String()
 	if len(code) == 11 {
-		// If 11 digits, return the format 123/456/78901
+		// If 11 digits, return the standard format 123/456/78901 (3/3/5)
 		code = fmt.Sprintf("%s/%s/%s", code[:3], code[3:6], code[6:])
 	} else if len(code) == 10 {
-		// If 10 digits, return the format 12/345/67890
+		// If 10 digits, return the format 12/345/67890 (2/3/5)
 		code = fmt.Sprintf("%s/%s/%s", code[:2], code[2:5], code[5:])
 	}
 	id.Code = cbc.Code(code)
