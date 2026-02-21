9eb62cb410376e52515e109da6f4115f2b6420cd
Mario Vidal Domínguez <60353145+Mariovido@users.noreply.github.com>
2026-02-21 17:19:29 +0100

    Fix codex comment

--- regimes/nz/tax_identity.go
+++ regimes/nz/tax_identity.go
@@ -16,6 +16,9 @@ var (
 
 // validateTaxIdentity checks to ensure the NZ IRD format is correct.
 func validateTaxIdentity(tID *tax.Identity) error {
+	if tID == nil {
+		return nil
+	}
 	return validation.ValidateStruct(tID,
 		validation.Field(&tID.Code, validation.By(validateIRDCode)),
 	)
