80e47a96e04d87f7f9be90fdf0b0eb0a83516ecf
Sam Lown <sam.lown@invopop.com>
2026-03-24 22:58:39 +0000

    Simplifying addons

--- addons/br/nfe/bill_line.go
+++ addons/br/nfe/bill_line.go
@@ -2,39 +2,20 @@ package nfe
 
 import (
 	"github.com/invopop/gobl/bill"
-	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/regimes/br"
 	"github.com/invopop/gobl/rules"
-	"github.com/invopop/gobl/rules/is"
 )
 
 func billLineRules() *rules.Set {
 	return rules.For(new(bill.Line),
 		rules.Assert("01", "line taxes must include the ICMS category",
-			is.Func("has ICMS", lineHasTaxCategory(br.TaxCategoryICMS)),
+			bill.RequireLineTaxCategory(br.TaxCategoryICMS),
 		),
 		rules.Assert("02", "line taxes must include the PIS category",
-			is.Func("has PIS", lineHasTaxCategory(br.TaxCategoryPIS)),
+			bill.RequireLineTaxCategory(br.TaxCategoryPIS),
 		),
 		rules.Assert("03", "line taxes must include the COFINS category",
-			is.Func("has COFINS", lineHasTaxCategory(br.TaxCategoryCOFINS)),
+			bill.RequireLineTaxCategory(br.TaxCategoryCOFINS),
 		),
 	)
-}
-
-// lineHasTaxCategory returns a function that checks whether a bill.Line
-// contains a tax combo with the given category code.
-func lineHasTaxCategory(cat cbc.Code) func(any) bool {
-	return func(val any) bool {
-		line, ok := val.(*bill.Line)
-		if !ok || line == nil {
-			return true
-		}
-		for _, tc := range line.Taxes {
-			if tc != nil && tc.Category == cat {
-				return true
-			}
-		}
-		return false
-	}
 }
--- addons/br/nfse/bill_line.go
+++ addons/br/nfse/bill_line.go
@@ -2,31 +2,14 @@ package nfse
 
 import (
 	"github.com/invopop/gobl/bill"
-	"github.com/invopop/gobl/cbc"
 	"github.com/invopop/gobl/regimes/br"
 	"github.com/invopop/gobl/rules"
-	"github.com/invopop/gobl/rules/is"
 )
-
-func lineHasTaxCategory(cat cbc.Code) func(any) bool {
-	return func(val any) bool {
-		line, ok := val.(*bill.Line)
-		if !ok || line == nil {
-			return true
-		}
-		for _, tc := range line.Taxes {
-			if tc != nil && tc.Category == cat {
-				return true
-			}
-		}
-		return false
-	}
-}
 
 func billLineRules() *rules.Set {
 	return rules.For(new(bill.Line),
 		rules.Assert("01", "line taxes must include the ISS category",
-			is.Func("has ISS", lineHasTaxCategory(br.TaxCategoryISS)),
+			bill.RequireLineTaxCategory(br.TaxCategoryISS),
 		),
 	)
 }
