26a51887739cf0a213a1bbc583f8288898fd809b
mrdanwa <wdanrui@gmail.com>
2026-09-17 16:30:03 +0000

    Move the Colombia DIAN addon to the gobl.co.dian module
    The co-dian-v2 addon now lives in github.com/invopop/gobl.co.dian and
    registers through the approved external addon list; the key remains a
    valid $addons value. The Colombian tax regime stays in core. The CO
    correction and stamp-copy tests move to the new module alongside the
    addon and its examples.

--- addons/addons.go
+++ addons/addons.go
@@ -10,7 +10,6 @@ package addons
 import (
 	// Import all the addons to ensure they're ready to use.
 	_ "github.com/invopop/gobl/addons/ar/arca"
-	_ "github.com/invopop/gobl/addons/co/dian"
 	_ "github.com/invopop/gobl/addons/de/xrechnung"
 	_ "github.com/invopop/gobl/addons/de/zugferd"
 	_ "github.com/invopop/gobl/addons/es/facturae"
